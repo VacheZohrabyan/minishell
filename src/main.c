0bfd30f3a88b0db908cbc81b7e678cee1fb1a7b2
VacheZohrabyan <vachezohrabyan218@gmail.com>
2025-08-23 13:42:45 +0400

    sd

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vzohraby <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 18:20:34 by vzohraby          #+#    #+#             */
-/*   Updated: 2025/08/23 13:26:32 by vzohraby         ###   ########.fr       */
+/*   Updated: 2025/08/23 13:42:11 by vzohraby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,53 +17,40 @@ int	main(int argc, char **argv, char **env)
 	t_env	*env_node;
 	t_token	*token;
 	char	*buffer;
-	pid_t pid;
 
 	env_node = NULL;
 	token = NULL;
 	buffer = NULL;
-	pid = fork();
 	if (argc != 1 || !argv[0])
 		return (0);
 	init_env(&env_node, env);
 	sig();
 	while (1)
 	{
-		if (pid < 0)
+		buffer = readline("minishell>"); // PROMPT
+		if (!buffer)
 		{
-			printf("chi exe");
+			printf("exit\n");
+			break ;
+		if (syntax(token, env_node))
+		{
+			printf("okey\n");
 		}
-		else if (pid == 0)
+		add_history(buffer);
+		token = lexical(buffer);
+		if (token)
 		{
-			buffer = readline("minishell>"); // PROMPT
-			if (!buffer)
-			{
-				printf("exit\n");
-				break ;
 			if (syntax(token, env_node))
 			{
+				check_builtin(token, env_node);
 				printf("okey\n");
 			}
-			add_history(buffer);
-			token = lexical(buffer);
-			if (token)
-			{
-				if (syntax(token, env_node))
-				{
-					check_builtin(token, env_node);
-					printf("okey\n");
-				}
-				token_node_free(&token);
-			}
-			else
-				printf("token_NULL\n");
-			free(buffer);
-			buffer = NULL;
+			token_node_free(&token);
 		}
 		else
-		{
-			wait(NULL);	
-		}
+			printf("token_NULL\n");
+		free(buffer);
+		buffer = NULL;
 	}
 	token_node_free(&token);
 	free_env(env_node);
